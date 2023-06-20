9d695632d292ec10b1af1ea936aae14cdeecc42c
Melchizedec_ <arno.ammiche@epitech.eu>
2023-06-20 14:21:06 +0200

    ADD: Battle system with UI Bar

---
+++ Assets/Scripts/Personnages/PlayerController1.cs
@@ -0,0 +1,69 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public Slider sliderUlti;
+    public PlayerController2 playerController;
+    public Slider sliderHP;
+    public GameManager manager;
+    public float speed = 1.0f;
+    public int joystickNumber;
+    public float AttackTimer;
+    public float AttackCooldown;
+    public int UltiXp_Current;
+    public int UltiXp_Need;
+    public int health;
+    public int damage;
+    private void Start()
+    {
+        SetMaxValueUlti(UltiXp_Need, UltiXp_Current);
+        SetMaxValueHP(health, health);
+    }
+    void Update()
+    {
+        SetValueHP(health);
+        SetValueUlti(UltiXp_Current);
+        float moveHorizontal = Input.GetAxis("Horizontal1");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        transform.Translate(movement * speed * Time.deltaTime);
+        if (manager.inFight)
+        {
+            AttackTimer += Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Fire1." + joystickNumber) && AttackTimer >= AttackCooldown)
+        {
+            Debug.Log("Attack 1er perso");
+            playerController.health -= damage;
+            UltiXp_Current += 5;
+        }
+        if (Input.GetButtonDown("LB" + joystickNumber) && UltiXp_Current >= UltiXp_Need)
+        {
+            Debug.Log("Ulti 1er perso");
+            playerController.health -= damage * 2;
+            UltiXp_Current = 0;
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject, 0.1f);
+        }
+    }
+    public void SetValueUlti(int value)
+    {
+        sliderUlti.value = value;
+    }
+    public void SetMaxValueUlti(int max, int min)
+    {
+        sliderUlti.maxValue = max;
+        sliderUlti.value = min;
+    }
+    public void SetValueHP(float value)
+    {
+        sliderHP.value = value;
+    }
+    public void SetMaxValueHP(float max, float min)
+    {
+        sliderHP.maxValue = max;
+        sliderHP.value = min;
+    }
+}
